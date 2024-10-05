a3d6069913873eaf0de03625733d1e0e487d425f
PerfectDevloper <hakobyanzaqar15@gmail.com>
2024-10-05 14:52:08 +0400

    Change to StandardJS style

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const electron = require('electron')
-const app = electron.app // Module to control application life
-const BrowserWindow = electron.BrowserWindow // Module to create native browser window.
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
 
 // Keep a global reference of the window object, if you don't the window will
 // be closed automatically when the JavaScript object is garbage collected.
-var mainWindow = null;
+let mainWindow = null;
 
 app.on('window-all-closed', function() {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform != 'darwin') {
+    if (process.platform !== 'darwin') {
         app.quit()
     }
 })
 
-app.on('ready', function() {
+app.on('ready', function () {
     mainWindow = new BrowserWindow({width: 1360, height: 800})
     mainWindow.loadURL('file://' + __dirname + '/index.html')
     mainWindow.webContents.openDevTools()
-    mainWindow.on('closed', function() {
+    mainWindow.on('closed', function () {
         mainWindow = null
     })
 })
