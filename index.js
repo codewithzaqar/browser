f6969a42da2f9b5acaeca6ec37922c6a86824ff7
PerfectDevloper <hakobyanzaqar15@gmail.com>
2024-10-05 14:47:15 +0400

    Create better shell template

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const BrowserWindow = electron.BrowserWindow // Module to create native browser 
 // be closed automatically when the JavaScript object is garbage collected.
 var mainWindow = null;
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function() {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
@@ -15,24 +14,11 @@ app.on('window-all-closed', function() {
     }
 })
 
-// This method will be called when Electron has finished
-// Initialization and is ready to create browser windows.
 app.on('ready', function() {
-    // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 600})
-
-    // and load the index.html of the app.
-    //mainWindow.loadURL('file://' + __dirname + '/index.html')
-    mainWindow.loadURL('http://google.com')
-
-    // Open the DevTools.
+    mainWindow = new BrowserWindow({width: 1360, height: 800})
+    mainWindow.loadURL('file://' + __dirname + '/index.html')
     mainWindow.webContents.openDevTools()
-
-    // Emitted when the window is closed.
     mainWindow.on('closed', function() {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
         mainWindow = null
     })
 })
