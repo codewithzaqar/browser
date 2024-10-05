a9326854f13c227c654dd15b9b9e5e0594d947aa
PerfectDevloper <hakobyanzaqar15@gmail.com>
2024-10-05 14:59:03 +0400

    Open dev tools always when in non production mode

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+'use strict'
 const electron = require('electron')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
@@ -17,7 +18,9 @@ app.on('window-all-closed', function() {
 app.on('ready', function () {
     mainWindow = new BrowserWindow({width: 1360, height: 800})
     mainWindow.loadURL('file://' + __dirname + '/index.html')
-    mainWindow.webContents.openDevTools()
+    if (!process.env.PRODUCTION) {
+        mainWindow.openDevTools()
+    }
     mainWindow.on('closed', function () {
         mainWindow = null
     })
